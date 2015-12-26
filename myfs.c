e49359cf3714b7e2714d74c6bfec91edf37f264a
san1805 <san1805@bk.ru>
2015-12-26 21:37:16 +0300

    add open func

--- myfs.c
+++ myfs.c
@@ -1,3 +1,22 @@
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+
+//открытие файла
+ 
+static int hello_open(const char *path, struct fuse_file_info *fi)
+{
+    if(strcmp(path, hello_path) != 0)
+        return -ENOENT;
+ 
+    if((fi->flags & 3) != O_RDONLY)
+        return -EACCES;
+ 
+    return 0;
+}
+
 static struct fuse_operations operations =  // структура нами операций
 {
     .getattr    = myfs_getattr,// атрибуты файла
